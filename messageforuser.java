e4ec32bf53b9835ccffcbecf1c66948b60b52581
manavkodes <manavkr18@gmail.com>
2026-05-24 00:28:38 +0530

    reading user input

--- messageforuser.java
+++ messageforuser.java
@@ -4,8 +4,8 @@ public class messageforuser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("write a message");
-        String msg = scanner.nextLine();
-        System.out.println(msg);
+        String mg = scanner.nextLine();
+        System.out.println(mg);
 
     }
 }
