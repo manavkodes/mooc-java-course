5c977d726ef226f6d4534b7e37366b7c26a740e0
manavkodes <manavkr18@gmail.com>
2026-05-24 00:39:25 +0530

    reading user input

---
+++ adalovelace.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class adalovelace {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String msg = scn.nextLine();
+        System.out.println("Hi "+msg+"!");
+
+    }
+}
