279301ba679c9b60ff41ed86d385cdf952263bd0
manavkodes <manavkr18@gmail.com>
2026-06-12 20:49:24 +0530

    reversing a string

---
+++ TESTing.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class TESTing {
+
+    public static void main(String[] args) {
+        ArrayList<String> word = new ArrayList<>();
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+            System.out.println("enter values");
+            String x = scn.nextLine();
+            if (x.equals("")) {
+                break;
+            }
+            word.add(x);
+
+
+        }
+        for (int i = word.size()-1;i>=0;i--) {
+            System.out.println(word.get(i));
+        }
+
+
+
+
+
+
+    }
+}
